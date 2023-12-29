9a2c250f6c056f18c698f64079cfe9ae5859d8d5
teohyiqin <155171060+teohyiqin@users.noreply.github.com>
2023-12-29 22:10:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cqmssbb7hq7u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "teohyiqin-employee"
+customregion = "us-east-1"
+
